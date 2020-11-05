5b023e0783b65b88d684a01ef77cab5ca06b34f1
Jackie Perez <67940416+jcquper@users.noreply.github.com>
2020-11-05 12:24:37 -0800

    Delete seeds.sql

--- seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name) VALUES (BBQ Western Cheeseburger);
-INSERT INTO burgers (burger_name) VALUES (Teriyaki Pineapple Burger);
-INSERT INTO burgers (burger_name) VALUES (Jalapeno Cheeseburger)
