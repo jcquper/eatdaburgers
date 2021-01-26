d1330d454c98fbc40c9867331a31e2e8564464ec
jcquper <jackie.perez334@gmail.com>
2021-01-26 10:42:55 -0800

    attempt #123456789"

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL){
     host: "localhost",
     user: "root",
     password: "password",
-    database: "burgers_db"
+    database: "burger_db"
   })
 }
  
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 USE burger_db;
 
 INSERT INTO burgers (burger_name)
-    VALUES
-("BBQ Western Cheeseburger"),
-("Teriyaki Pineapple Burger"),
-("Jalapeno Cheeseburger");
+	VALUES
+		("BBQ Western Cheeseburger"),
+		("Teriyaki Pineapple Burger"),
+		("Jalapeno Cheeseburger");
